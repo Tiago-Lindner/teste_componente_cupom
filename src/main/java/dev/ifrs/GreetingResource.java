154f277af7e467c1a297602c20f1ad518c2eac5b
Tiago-Lindner <tiago.f.lindner@gmail.com>
2023-04-24 11:38:37 -0300

    a

---
+++ src/main/java/dev/ifrs/GreetingResource.java
@@ -0,0 +1,27 @@
+package dev.ifrs;
+
+import javax.ws.rs.POST;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.core.MediaType;
+
+@Path("/cupom")
+public class GreetingResource {
+
+    @POST
+    @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String calculaCupom(@FormParam("valor") float valor, @FormParam("cupom") String cupom) {
+        if (cupom.equals("CUPOM10")){
+            valor = (valor/10) * 9;
+            return String.valueOf(valor);
+        } else if (cupom.equals("CUPOM15")){
+            valor = (valor/100) * 85;
+            return String.valueOf(valor);
+        } else {  
+            return String.valueOf(valor);
+        }
+    }
+}
